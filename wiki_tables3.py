4d3e3d6a6e40788a2fcefce6c086f27981764fd3
Reimu <yunihikokun@gmail.com>
2018-05-04 02:11:34 +0500

    123
    123

---
+++ wiki_tables3.py
@@ -0,0 +1,44 @@
+import csv
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+def steal_tables(url):
+    s_tables = []
+    html = urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    tables = soup.find_all('table', 'wikitable')
+    for table in tables:
+        s_table = []
+        rows = table.find_all('tr')
+        for row in rows:
+            s_row = []
+            for cell in row.find_all(['td', 'th']):
+                s_cell = cell.get_text()
+                s_row.append(s_cell)
+            s_table.append(s_row)
+        s_tables.append(s_table)
+    return s_tables
+
+
+def joining_tables(tables):
+    pass
+
+
+def show_result(table):
+    pass
+
+
+def main():
+    print('enter url')
+    url = input()
+    print('stealing tables')
+    tables = steal_tables(url)
+    print('joining tables')
+    table = joining_tables(tables)
+    print('complete')
+    show_result(table)
+
+
+if __name__ == "__main__":
+    main()
