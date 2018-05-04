8129900f42caeb6abb3ed7db0590f0ad3da07a32
Reimu <yunihikokun@gmail.com>
2018-05-04 12:05:15 +0500

    ваыыф

--- wiki_tables3.py
+++ wiki_tables3.py
@@ -10,19 +10,23 @@ def steal_tables(url):
     tables = soup.find_all('table', 'wikitable')
     for table in tables:
         s_table = []
+        for row in table.find_all('tr', 'sortbottom'):
+            row.decompose()
         rows = table.find_all('tr')
         for row in rows:
             s_row = []
             for cell in row.find_all(['td', 'th']):
                 try:
                     cell.sup.decompose()
-                except Exception:
+                except AttributeError:
                     pass
+
                 s_cell = cell.get_text()
                 s_row.append(s_cell)
             s_table.append(s_row)
         s_tables.append(s_table)
     return s_tables
+
 
 
 def joining_tables(tables):
